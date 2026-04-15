f5d83055c27476887b483ae1b5c4e2b445a1b6b9
gui <guilhermerrtostes@gmail.com>
2026-04-15 12:27:57 -0300

    acabou full

--- main.py
+++ main.py
@@ -28,7 +28,6 @@ batman_font = font.Font("fontecarai.otf", 40)
 mixer.music.load("matue.mp3")
 mixer.music.set_volume(0.3)
 mixer.music.play(-1)
-
 feijao = mixer.Sound("feijao-com-farinha.mp3")
 baby1 = mixer.Sound("baby1.mp3")
 baby2 = mixer.Sound("baby2.mp3")
@@ -43,7 +42,7 @@ iii=188
 ooo=99
 background_color = (uuu, iii, ooo)
 
-
+modo_mouse = False
 
 aaa = (uuu-(600//5.75))
 bbb = (iii+(600//25))
@@ -64,6 +63,10 @@ while running:
             key_pressed = ev.key
             if key_pressed == K_SPACE:
                 modo_fundo = not modo_fundo
+        if ev.type == KEYDOWN:
+            key_pressed = ev.key
+            if key_pressed == K_x:
+                modo_mouse = not modo_mouse
 
 
         if ev.type == MOUSEBUTTONDOWN:
@@ -91,62 +94,84 @@ while running:
         if modo_fundo:
             background_color = (245,178,64)
         else:
-                if raio_x < 600:
-                    background_color = (uuu-(raio_x)//5.75, iii+(raio_x//25), ooo+(raio_x//3.95))
+                if modo_mouse == False:
+                    if raio_x < 600:
+                        background_color = (uuu-(raio_x)//5.75, iii+(raio_x//25), ooo+(raio_x//3.95))
+                    else:
+                        if raio_x >= 600:
+                            background_color = (aaa-((raio_x-600)//5), bbb-((raio_x-600)//4.3), ccc-((raio_x-600)//4.5))
                 else:
-                    if raio_x >= 600:
-                        background_color = (aaa-((raio_x-600)//5), bbb-((raio_x-600)//4.3), ccc-((raio_x-600)//4.5))
+                    if modo_mouse == True:
+                        if raio_x < 600:
+                            background_color = (uuu-(raio_x)//5.75, iii+(raio_x//25), ooo+(raio_x//3.95))
+                        else:
+                            if raio_x >= 600:
+                                background_color = (aaa-((raio_x-600)//5), bbb-((raio_x-600)//4.3), ccc-((raio_x-600)//4.5))
 
-    
-    print(background_color)
+    #if modo_mouse == True:
+        #print(mouse_x)
+    #print(background_color)
 
 
     ##update
     dt = clock.get_time()/1000
     keys = key.get_pressed()
 
-
-
+    mouse_x, mouse_y = mouse.get_pos()
 
 
 
     #teclas
-    if raio_x <1350:
+
+    if modo_mouse == True:
+        raio_x = (mouse_x)
+
+
+    #if modo_mouse == False:
+    if raio_x<1350:
         if keys[K_d]:
-            raio_x = raio_x + 100 *dt
+                raio_x = raio_x + 100 *dt
     if raio_x > 10:
         if keys[K_a]:
-            raio_x = raio_x - 100 *dt
-
+                raio_x = raio_x - 100 *dt
     if raio_x <1350:
         if ev.type == MOUSEBUTTONDOWN:
             if ev.button == 1:
                 raio_x = raio_x + 100 *dt
-    
     if raio_x > -50:
         if ev.type == MOUSEBUTTONDOWN:
             if ev.button == 3:
                 raio_x = raio_x - 100 *dt
+    
+    
+
 
 
     #desenhar a partir daqui
 
     window.fill(background_color)
 
-
-    draw.line(window, (255, 242, 81), (raio_x - 60,130), (raio_x - 120,130), 10)
-    draw.line(window, (255, 242, 81), (raio_x +60 ,130), (raio_x + 120,130), 10)
-    draw.line(window, (255, 242, 81), (raio_x,190), (raio_x,250), 10)
-    draw.line(window, (255, 242, 81), (raio_x,70), (raio_x,10), 10)
+    if modo_mouse == False:
+        draw.line(window, (255, 242, 81), (raio_x-60,130), (raio_x-120,130), 10)
+        draw.line(window, (255, 242, 81), (raio_x+60 ,130), (raio_x+120,130), 10)
+        draw.line(window, (255, 242, 81), (raio_x,190), (raio_x,250), 10)
+        draw.line(window, (255, 242, 81), (raio_x,70), (raio_x,10), 10)
+        draw.circle(window, (255, 242, 81), (raio_x,130), 60)    
     draw.rect(window, (101, 67, 33), (1000, 240, 80, 400))
     draw.circle(window, (0, 128, 0), (1040,300), 100)
     draw.rect(window, (72, 157, 37), (0,600,1280,220))
     draw.rect(window, (100, 100, 100), (350, 360,200,240))
-    draw.circle(window, (255, 242, 81), (raio_x,130), 60)
     draw.polygon(window, (242, 136, 59), ((350,360),(550,360),(450,200)))
     draw.rect(window, (13, 22, 100), (370, 460,50,70))
     draw.rect(window, (121, 77, 27), (450, 430,75,170))
     draw.circle(window, (0,0,0), (470,520), 7)
+
+    if modo_mouse == True:
+        draw.line(window, (255, 242, 81), (mouse_x-60,mouse_y), (mouse_x-120,mouse_y), 10)
+        draw.line(window, (255, 242, 81), (mouse_x+60 ,mouse_y), (mouse_x+120,mouse_y), 10)
+        draw.line(window, (255, 242, 81), (mouse_x,mouse_y+60), (mouse_x,mouse_y+120), 10)
+        draw.line(window, (255, 242, 81), (mouse_x,mouse_y-60), (mouse_x,mouse_y-120), 10)
+        draw.circle(window, (255, 242, 81), (mouse_x,mouse_y), 60)    
 
     #nuvem
     nuvem_x += velocidade_nuvem
